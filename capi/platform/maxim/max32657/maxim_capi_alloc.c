8f7582d386fd7b4cef4004c68068e9a4c35c07db
Ramon Miguel Imbao <ramonmiguel.imbao@analog.com>
2026-07-01 08:46:16 +0800

    capi: platform: maxim: max32657: Add CAPI alloc and free functions
    Add implementation for CAPI alloc and free functions
    
    Signed-off-by: Ramon Miguel Imbao <ramonmiguel.imbao@analog.com>

---
+++ capi/platform/maxim/max32657/maxim_capi_alloc.c
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *   @file   maxim_capi_alloc.c
+ *   @brief  Override of weak CAPI alloc functions
+ *   @author Ramon Miguel Imbao (ramonmiguel.imbao@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
+
+#include "capi_alloc.h"
+
+void *capi_malloc_impl(size_t size)
+{
+	return malloc(size);
+}
+
+void *capi_calloc_impl(size_t num, size_t size)
+{
+	return calloc(num, size);
+}
+
+void capi_free_impl(void *ptr)
+{
+	free(ptr);
+}
